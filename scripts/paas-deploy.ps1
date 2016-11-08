cd3185b6fccf072273e9481d7f3fa67b72e53426
Oyeniyi Oyelade <niyioyelade@yahoo.com>
2016-11-08 17:03:56 +0000

    5309: Add powershell clound foundry deployment script for the bridge
    @Author: @niyioyelade

--- scripts/paas-deploy.ps1
+++ scripts/paas-deploy.ps1
@@ -12,3 +12,11 @@ cf push stub-sp-reference           -f "$scripts_dir/default-manifest.yml"      
 cf push uk-connector-node-reference -f "$scripts_dir/connector-node-manifest.yml" -p "$project_root/EIDAS-Node/target/EidasNode.war"
 cf push es-proxy-node-reference     -f "$scripts_dir/proxy-node-manifest.yml"     -p "$project_root/EIDAS-Node/target/EidasNode.war"
 cf push stub-idp-reference          -f "$scripts_dir/default-manifest.yml"        -p "$project_root/EIDAS-IdP-1.0/target/IdP.war"
+
+
+#deploy bridge
+$bridge_parent = Split-Path -parent $project_root
+$bridge_root = "$bridge_parent/verify-eidas-bridge"
+cd $bridge_root
+./gradlew distZip
+cf push eidas-bridge -p build/distributions/verify-eidas-bridge.zip -f bridge-manifest.yml
