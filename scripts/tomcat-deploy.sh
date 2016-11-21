6477926a8a72d5bb9c9484e8db1d4618f0e1e03b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-11-21 14:18:51 +0000

    Revert "5220: Switch back to using the test keystore when running locally"
    This reverts commit 828eab1437f0409feddf1068a4019dbe60db7244.

--- scripts/tomcat-deploy.sh
+++ scripts/tomcat-deploy.sh
@@ -34,11 +34,22 @@ cp "$project_root"/EIDAS-SP/target/SP.war "$CATALINA_HOME/webapps"
 # Deploy the Connector Node
 cp "$project_root"/EIDAS-Node/target/EidasNode.war "$CATALINA_HOME/webapps/ConnectorNode.war"
 
+# Deploy the IdP
+cp "$project_root"/EIDAS-IdP-1.0/target/IdP.war "$CATALINA_HOME/webapps"
+
+# Hack - reconfigure the Node to be a proxy node instead of a connector node
+FILES_TO_REPLACE=$(git grep '${NODE_' | cut -d: -f1 | grep .xml | sort -u)
+for file in $FILES_TO_REPLACE; do
+  sed -i '.original' 's/${NODE_/${PROXY_NODE_/g' $file
+done
+
+mvn --file EIDAS-Parent clean install -P embedded -P coreDependencies -Dmaven.test.skip=true
+
 # Deploy the Proxy Node
 cp "$project_root"/EIDAS-Node/target/EidasNode.war "$CATALINA_HOME/webapps/ProxyNode.war"
 
-# Deploy the IdP
-cp "$project_root"/EIDAS-IdP-1.0/target/IdP.war "$CATALINA_HOME/webapps"
+# Restore the modified files from their backups
+find . -name "*.original" -exec sh -c 'mv -f $0 ${0%.original}' {} \;
 
 # ---------------------------
 # Environment Variables
@@ -47,30 +58,42 @@ cp "$project_root"/EIDAS-IdP-1.0/target/IdP.war "$CATALINA_HOME/webapps"
 export EIDAS_CONFIG_REPOSITORY="$project_root"/EIDAS-Config/
 
 # Stub SP
-export STUB_SP_KEYSTORE="$project_root/EIDAS-SP/target/SP/WEB-INF/eidasKeystore.jks"
-export STUB_SP_KEYSTORE_PASSWORD="local-demo"
-export STUB_SP_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
-export STUB_SP_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="54d8a000"
-export STUB_SP_SIGNING_CERTIFICATE_DISTINGUSHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
-export STUB_SP_SIGNING_CERTIFICATE_SERIAL_NUMBER="54d8a000"
+export STUB_SP_KEYSTORE="$project_root/EIDAS-Node/target/EidasNode/WEB-INF/stubSpKeystore.jks"
+export STUB_SP_KEYSTORE_PASSWORD="Password"
+export STUB_SP_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Stub SP Encryption 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export STUB_SP_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="41573ab34ef7100cb77e4c5aca551826cf86a1e9"
+export STUB_SP_SIGNING_CERTIFICATE_DISTINGUSHED_NAME="CN=Test Stub SP Metadata Signing 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export STUB_SP_SIGNING_CERTIFICATE_SERIAL_NUMBER="123f7141d99aa3b6ad31211dabb9ca7abdc08b5d"
 
-# Nodes
-export NODE_KEYSTORE="$project_root/EIDAS-Node/target/EidasNode/WEB-INF/eidasKeystore.jks"
-export NODE_KEYSTORE_PASSWORD="local-demo"
+# Connector Node
+export NODE_KEYSTORE="$project_root/EIDAS-Node/target/EidasNode/WEB-INF/connectorNodeKeystore.jks"
+export NODE_KEYSTORE_PASSWORD="Password"
 
-export NODE_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
-export NODE_SIGNING_CERTIFICATE_DISTINGUISHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
+export NODE_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Connector Encryption 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export NODE_SIGNING_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Connector Metadata Signing 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
 
-export NODE_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="54d8a000"
-export NODE_SIGNING_CERTIFICATE_SERIAL_NUMBER="54d8a000"
+export NODE_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="1dcfdeedc8983a5f13f2338e0814b6e47090b3d7"
+export NODE_SIGNING_CERTIFICATE_SERIAL_NUMBER="56520de46a76cb6ad7b9c238dd253d88904da9d8"
+
+# Proxy Node
+# Hack: PROXY_* variables are only needed for running locally due to instances running in
+# the same application and therefore getting the same environment variables:
+export PROXY_NODE_KEYSTORE="$project_root/EIDAS-Node/target/EidasNode/WEB-INF/proxyNodeKeystore.jks"
+export PROXY_NODE_KEYSTORE_PASSWORD="Password"
+
+export PROXY_NODE_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Proxy Encryption 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export PROXY_NODE_SIGNING_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Proxy Metadata Signing 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+
+export PROXY_NODE_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="6641716bee633fb618dbd85b7d41e63b62046c2d"
+export PROXY_NODE_SIGNING_CERTIFICATE_SERIAL_NUMBER="203b6cb0714922c675e08606187e75c4c4457a1c"
 
 # Stub IdP
-export STUB_IDP_KEYSTORE="$project_root/EIDAS-IdP-1.0/target/IdP/WEB-INF/eidasKeystore.jks"
-export STUB_IDP_KEYSTORE_PASSWORD="local-demo"
-export STUB_IDP_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
-export STUB_IDP_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="54d8a000"
-export STUB_IDP_SIGNING_CERTIFICATE_DISTINGUISHED_NAME="CN=local-demo-cert, OU=DIGIT, O=European Comission, L=Brussels, ST=Belgium, C=BE"
-export STUB_IDP_SIGNING_CERTIFICATE_SERIAL_NUMBER="54d8a000"
+export STUB_IDP_KEYSTORE="$project_root/EIDAS-Node/target/EidasNode/WEB-INF/stubIdpKeystore.jks"
+export STUB_IDP_KEYSTORE_PASSWORD="Password"
+export STUB_IDP_ENCRYPTION_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Stub IdP Encryption 20161031, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export STUB_IDP_ENCRYPTION_CERTIFICATE_SERIAL_NUMBER="723c0f7f4b45dea5296e53f565535174d0df5218"
+export STUB_IDP_SIGNING_CERTIFICATE_DISTINGUISHED_NAME="CN=Test Stub IdP Metadata Signing 20161026, OU=Government Digital Service, O=Cabinet Office, L=London, ST=Greater London, C=UK"
+export STUB_IDP_SIGNING_CERTIFICATE_SERIAL_NUMBER="5ae2153c3f9a99824df394abd65f9e1e8ca53365"
 
 # URLs
 export SP_URL='http://127.0.0.1:8080/SP'
@@ -85,4 +108,3 @@ export IDP_SSO_URL='https://127.0.0.1:8080/IdP'
 # ---------------------------
 
 catalina run
-
